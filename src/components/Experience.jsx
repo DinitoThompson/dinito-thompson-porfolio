f59f40c7f2d3ae84d0a9e51c3aafcecb6fcdda45
DenitoSenPai <dinitothompson@gmail.com>
2022-08-09 18:00:08 -0500

    Altered Experience Pop Up

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -241,7 +241,7 @@ const SkillSet = () => {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <div className="fixed inset-0 bg-gradient-to-t from-gray-900 to-black bg-opacity-70 blur-md" />
+              <div className="fixed inset-0 bg-gradient-to-bl from-orange-900 via-black to-black bg-opacity-70 blur-md" />
             </Transition.Child>
 
             <div className="fixed inset-0 overflow-y-auto">
